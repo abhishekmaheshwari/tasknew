d44ec27b6abfd014a3c201fe896924d2488f18a7
Abhishek Maheshwari <a.amaheshwari@gmail.com>
2024-02-06 09:57:59 +0530

    new changes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const dotenv = require('dotenv').config();
+const connectDB = require('./connect/database');
+const Cors = require('cors');
+const port = process.env.PORT || 5000;
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(Cors());
+
+app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
